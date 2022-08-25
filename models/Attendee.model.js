e195708b3f80ac1fe2cd77c68a8780bafb8f5b64
Agash Jeyakumar <agash.jey.it@gmail.com>
2022-08-25 12:22:23 +0200

    created Attendee and Option models

---
+++ models/Attendee.model.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require("mongoose");
+
+const attendeeSchema = new Schema({
+  event: {
+    type: Schema.Types.ObjectId,
+    ref: "Event",
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  availabilities: [Schema.Types.Date],
+  budget: Schema.Types.Number,
+  location: [Schema.Types.String],
+});
+
+const Attendee = model("Attendee", attendeeSchema);
+
+module.exports = Attendee;
