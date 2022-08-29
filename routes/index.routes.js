03b4bfc9ac30b2606def4cc832982821f90ff9c5
Agash Jeyakumar <agash.jey.it@gmail.com>
2022-08-29 10:25:50 +0200

    routes to display one event by Id , display 1 event by name, create attendee, log in, create votes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,4 +1,8 @@
 const router = require("express").Router();
+
+const jsonWebToken = require('jsonwebtoken');
+
+
 const Attendee = require("../models/Attendee.model");
 const User = require("../models/User.model");
 const Vote = require("../models/Vote.model");
@@ -46,7 +50,7 @@ router.post('/sign-in', async (req, res, next) => {
 router.post();
 
 //Invite people --> create attendee document - Kash
-router.post('/attendee', async (req, res, next) => {
+router.post('/event/attendee', async (req, res, next) => {
   try {
     const {event, user} = req.body;
     const createdAttendee = await Attendee.create({event, user});
@@ -61,7 +65,7 @@ router.post('/attendee', async (req, res, next) => {
 router.post();
 
 //vote creation - Kash
-router.post('/vote', async (req, res, next) => {
+router.post('/event/votes', async (req, res, next) => {
   try {
     const {attendee, firstChoice, secondChoice, thirdChoice} = req.body;
     const createdVote = await Vote.create({attendee, firstChoice, secondChoice, thirdChoice});
@@ -74,61 +78,97 @@ router.post('/vote', async (req, res, next) => {
 //add friends - Lau
 router.post();
 
-// display user friends
-router/this.get()
-
-//display one event by ID (with attendees, options, votes, etc)
+// display user friends - Lau
 router.get()
 
-//display one event by name 
-router.get()
-
-//display one option (with option ID and current votes)
-router.get()
-
-//display all events linked to one user - Kash
-router.get('/event/:idUser', async (req, res, next) => {
+//display one event by ID (with attendees, options, votes, etc) - Kash
+router.get('/events/:idEvent', isLoggedIn, async (req, res, next) => {
   try{
-    const idUser = req.params;
+    const { idEvent } = req.params;
+    const findEvent = await Event.findById({ _id : idEvent });
+    const findOptions = await Option.find({ event : idEvent });
+   
+    //findAttendees is an Array
+    const findAttendees = await Attendee.find({event : idEvent});
+
+    //findVotes is an array of objects
+    let findVotes = [];
+    //For one attendee of findAttendees, 
+    //I'll find his 3 votes and store them in votesOf.votes
+    //Each attendee will have his own object named votesOf
+    //and I'll store every votesOf objects in findVotes
+    for (let attendee of findAttendees){
+      const votesOf = {
+        attendee : attendee.name,
+        votes : [],
+      };
+      const findVotesOf = await Vote.find({attendee : attendee._id});
+      votesOf.votes.push(findVotesOf);
+      findVotes.push(votesOf);
+    }
+
+    const displayEvent = [findEvent, findOptions, findAttendees, findVotes];
+
+
+    res.json(displayEvent);
   }catch(error){
+    next(error);
+  }
+})
+
+//display one event by name - Kash
+router.get('/events', isLoggedIn, async (req, res, next) => {
+  try{
+    //comment recupérer l'id de l'user connecté ?
+    //car je dois retourner que les events de cet User
+    const {name} = req.body;
+    const displayEvent = await Event.find({name});
+    res.json(displayEvent);
+  } catch(error){
     next(error);
   }
 });
 
-//display all events - User role = admin
+//display one option (with option ID and current votes) - Lau
 router.get()
 
-//display all events - User role = attendee (notAdmin)
+//display all events linked to one user - Kash
+router.get('/events/:token');
+
+//display all events - User role = admin - Lau
 router.get()
 
-// display all upcoming events linked to a user
+//display all events - User role = attendee (notAdmin) - Lau
 router.get()
 
-//delete event
+// display all upcoming events linked to a user - Kash
+router.get()
+
+//delete event - Lau 
 router.delete()
 
-//delete options from event
+//delete options from event - Kash
 router.delete()
 
-//remove attendee from an event (isAdmin = true)
+//remove attendee from an event (isAdmin = true) - Lau
 router.delete()
 
-// remove user from friend list
+// remove user from friend list - Kash
 router.delete()
 
-//Update event informations
+//Update event informations - Lau
 router.patch()
 
-//Update options informations (isAdmin = true)
+//Update options informations (isAdmin = true) - Kash
 router.patch()
 
-//Update user profile informations
+//Update user profile informations - Lau
 router.patch()
 
-//modify vote
+//modify vote - Kash
 router.patch()
 
-//log out
+//log out - Lau
 router.post()
 
 
